debe30d1d11e823195bb43a390ce15a33a524843
Edward Nicholes Jr <GameDevFox@gmail.com>
2018-09-02 14:35:22 -0500

    Update Docker config

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM nginx
+FROM nginx:alpine
 
 ADD ./docker/default.conf /etc/nginx/conf.d/default.conf
 ADD ./docker/init /init
 
 WORKDIR /usr/share/nginx/html
 
-ADD ./dist/index.html .
-ADD ./dist/main.js .
+ADD ./dist .
 
 CMD /init
--- src/app/everempire-api.js
+++ src/app/everempire-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import config from './config';
 
-const http = axios.create({ baseURL: config.baseURL });
+const http = axios.create({ baseURL: config.apiURL });
 http.interceptors.response.use(result => result.data);
 
 export const login = (email, pass) => http.post('/login', { email, pass });
